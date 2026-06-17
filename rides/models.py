1c8c6cae7b6aaaaeb04eb8a3df17bcd9542cf703
ferjohn-amistad <amistadferjohn@gmail.com>
2026-06-17 23:36:40 +0800

    feat: adding authentication and admin permissions

--- rides/models.py
+++ rides/models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(models.Model):
+class User(AbstractUser):
     ROLES = [
         ('admin', 'Admin'),
         ('rider', 'Rider'),
